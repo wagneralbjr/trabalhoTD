64d4958eb5dedc13908beda315b2de1ca543ace2
wagneralbjr <wagneralbjr@gmail.com>
2018-06-10 16:18:37 -0300

    agora ta funfando haha

--- utils.py
+++ utils.py
@@ -11,21 +11,26 @@ def len_to_64b(nome):
 """ Esta função deve enviar tamanho e o arquivo somente."""
 def envia_arquivo(socket, arquivo_path, chunk_size = 1024):
 
+
     buffer = open(arquivo_path, "rb").read()
     tam_arquivo = len_to_64b(buffer)
 
+    print(f'enviando arquivo de {tam_arquivo}')
     print(tam_arquivo)
     socket.send(tam_arquivo.encode())
 
     tam_arquivo = len(buffer)
 
     qtd_packages = int( tam_arquivo / chunk_size)
+    print(f'o arquivo de {tam_arquivo} foi dividido em {qtd_packages}')
     for i in range(0 , qtd_packages):
         socket.send(buffer[i * chunk_size : (i+1) * chunk_size])
 
-    socket.send(buffer[(qtd_packages+1) * chunk_size : ])
+    indice_restante  = (qtd_packages) * chunk_size
+    print(f'indice restante {indice_restante}')
+    socket.send(buffer[indice_restante: ])
 
-    socket.send('0'.encode())
+    #socket.send('0'.encode())
 
     return
 
@@ -59,11 +64,12 @@ def recebe_arquivo(socket, chunk_size = 1024):
 
     arquivo = []
 
+    print(f'baixando arquivo de {tam_arquivo} bits')
     for i in range(0, qtd_packages):
 
         arquivo.append(socket.recv(chunk_size))
 
-    falta = tam_arquivo - (qtd_packages)
+    falta = tam_arquivo - (qtd_packages +1  * chunk_size )
 
     if (falta > 0):
         arquivo.append(socket.recv(falta))
